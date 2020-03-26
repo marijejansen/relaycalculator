d8128fffe3937172cc7dc21071d176d638f8c2d4
marije <marije.jansen@winvision.nl>
2020-03-26 11:06:30 +0100

    style

--- src/components/calculate/calc-result.vue
+++ src/components/calculate/calc-result.vue
@@ -2,7 +2,7 @@
         <span class="row" display: >
           <div class="age">{{team.age}}+</div>
           <div class="gender">{{gender}}</div>
-          <div class="rel-names" style="display:flex">
+          <div class="names" style="display:flex">
             <single-result-name v-for="(swimmer, index) in swimmers" :key='swimmer.id + "-" + index'  :relay-swimmer="swimmer" :first="index == 0" style="flex:1; display:flex"></single-result-name>
           </div>
           <div class="time">{{time}}</div>
